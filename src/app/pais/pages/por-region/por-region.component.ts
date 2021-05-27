c247b5a3220f5257ac32566f6a818b61c3ac0dae
LeoGarcia <leo26275@gmail.com>
2021-05-27 09:28:32 +0200

    Search by region module

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styles: [
+    `
+      button{
+        margin-right: 5px;
+      }
+    `
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+
+  regionActiva: string = '';
 
   constructor() { }
 
-  ngOnInit(): void {
+  getClaseCSS( region: string ): string{
+    return (this.regionActiva === region) ? 'btn btn-primary' : 'btn btn-outline-primary'
+  }
+  activarRegion( region: string ){
+    this.regionActiva = region;
+    
+    //TODO: hacer el llamado al servicio
   }
 
 }
